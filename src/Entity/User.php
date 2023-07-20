84c8ce6338506a61ed57ee56956518c71cd26226
tonytillet <tony.tillet@gmail.com>
2023-07-20 16:10:32 +0200

    User role management

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -227,7 +227,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getRoles(): array
     {
-        return ['ROLE_USER'];
+        return [$this->role];
     }
 
     public function eraseCredentials(): void
