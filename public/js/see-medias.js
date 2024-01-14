829b4538ba63849405a3d146f39cedf0f27a9e28
TonyTillet <tony@wagaia.com>
2024-01-14 14:36:30 +0100

    update :  see medias mobile btn

---
+++ public/js/see-medias.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#btn-images").click(function() {
+        $('.images-desktop').toggleClass("active"); // Remplacez "btn-primary" par la classe que vous souhaitez basculer.
+    });
+});
